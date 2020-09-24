def65851f1f2db1a84a12bb1a62fd0242123bd76
Mei Xuesong <meixuesong@gmail.com>
2020-09-24 16:37:26 +0800

    init project

---
+++ src/main/java/refactoring/kata/kanban/ColumnService.java
@@ -0,0 +1,12 @@
+package refactoring.kata.kanban;
+
+import java.util.List;
+
+/**
+ * @author  <a href="mailto:meixuesong@gmail.com">Mei Xuesong</a>
+ */
+public class ColumnService {
+    public List<TaskInfo> getTaskStatesByTeamId(String teamId) {
+        return null;
+    }
+}
